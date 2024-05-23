0a57ff60aedd3830542563b910caa828533e8683
Kai Fuller <kaifuller1995@gmail.com>
2024-05-22 21:54:13 -0600

    updated prompt

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 
 function makeDict(array) {
-  let fxnDict = {}
+  let fxnDict = {};
 
   for (let i=0; i < array.length; i++) {
 
@@ -8,14 +8,14 @@ function makeDict(array) {
       fxnDict[array[i]] += 1;
     } else {
       fxnDict[array[i]] = 1;
-    }
-  }
+    };
+  };
   return fxnDict
 }
 
 
-let orderStr = prompt("Please place your order separated by a comma")
-let orderList = orderStr.split(",")
-let orderDict = makeDict(orderList)
-console.log(orderDict)
+let orderStr = prompt("Please enter your froyo flavors: Separated by commas");
+let orderList = orderStr.split(",");
+let orderDict = makeDict(orderList);
+console.log(orderDict);
 
